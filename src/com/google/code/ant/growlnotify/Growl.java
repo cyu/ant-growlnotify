0aac61510170b5976bd7fb226b505db716c9c3ac
ketanpadegaonkar <ketanpadegaonkar@e52106af-3151-0410-b009-3db572bc262b>
2008-07-22 04:12:05 +0000

    backport to 1.4!

--- src/com/google/code/ant/growlnotify/Growl.java
+++ src/com/google/code/ant/growlnotify/Growl.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 
 class Growl {
 
-    public void sendNotification(GrowlNotification notification) throws GrowlException {
-        ArrayList<String> args = new ArrayList<String>();
-        args.add("growlnotify");
-        args.add("-m");
-        args.add(notification.msg);
-        args.add("-n");
-        args.add(notification.appName);
-        args.add("-t");
-        args.add(notification.title);
-        if (notification.sticky)
-            args.add("-s");
-        try {
-            Process exec = Runtime.getRuntime().exec(args.toArray(new String[] {}));
-        } catch (IOException e) {
-            throw new GrowlException(e);
-        }
-    }
+	public void sendNotification(GrowlNotification notification) throws GrowlException {
+		ArrayList args = new ArrayList();
+		args.add("growlnotify");
+		args.add("-m");
+		args.add(notification.msg);
+		args.add("-n");
+		args.add(notification.appName);
+		args.add("-t");
+		args.add(notification.title);
+		if (notification.sticky)
+			args.add("-s");
+		try {
+			Process exec = Runtime.getRuntime().exec((String[]) args.toArray(new String[] {}));
+		} catch (IOException e) {
+			throw new GrowlException(e);
+		}
+	}
 }
